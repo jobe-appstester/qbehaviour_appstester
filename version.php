4fbeeed583c3b364ebaa8a762976a3f0bcf6a27b
semen.lavren <semen.lavren@partner.samsung.com>
2023-01-10 09:55:49 +0300

    Same and complete response check, setup for regrades

--- version.php
+++ version.php
@@ -3,6 +3,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qbehaviour_appstester';
-$plugin->version = 2022120800;
+$plugin->version = 2022122900;
 $plugin->maturity = MATURITY_BETA;
 $plugin->release = 'v0.01';

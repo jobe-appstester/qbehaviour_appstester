b56db120767e53fdef355d3b1fb46eb2b21b2c43
XiRouz <xirouz@gmail.com>
2023-04-12 12:26:44 +0300

    Bump version

--- version.php
+++ version.php
@@ -3,6 +3,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qbehaviour_appstester';
-$plugin->version = 2023020800;
+$plugin->version = 2023041200;
 $plugin->maturity = MATURITY_BETA;
 $plugin->release = 'v0.01';
